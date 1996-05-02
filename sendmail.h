353564e3a57629f53cca3db897a619e4d163b28d
Todd Derr <tjd@wordsmith.org>
1996-05-02 22:39:42 +0000

    removed all dependencies on useful.h - accidentially missed this file in r40
    git-svn-id: file:///home/tjd/svn/mailer@41 ce0fb077-1888-4252-a13e-97717afc2b1d

--- sendmail.h
+++ sendmail.h
@@ -59,7 +59,49 @@ static char SmailSccsId[] =	"@(#)sendmail.h	8.43.1.3		3/5/95";
 # include <errno.h>
 
 # include "conf.h"
-# include "useful.h"
+
+# include <sys/types.h>
+
+/* support for bool type */
+typedef char	bool;
+# define TRUE	1
+# define FALSE	0
+
+# ifndef NULL
+# define NULL	0
+# endif /* NULL */
+
+/* bit hacking */
+# define bitset(bit, word)	(((word) & (bit)) != 0)
+
+/* some simple functions */
+# ifndef max
+# define max(a, b)	((a) > (b) ? (a) : (b))
+# define min(a, b)	((a) < (b) ? (a) : (b))
+# endif
+
+/* assertions */
+# ifndef NASSERT
+# define ASSERT(expr, msg, parm)\
+	if (!(expr))\
+	{\
+		fprintf(stderr, "assertion botch: %s:%d: ", __FILE__, __LINE__);\
+		fprintf(stderr, msg, parm);\
+	}
+# else /* NASSERT */
+# define ASSERT(expr, msg, parm)
+# endif /* NASSERT */
+
+/* sccs id's */
+# ifndef lint
+#  ifdef __STDC__
+#   define SCCSID(arg)	static char SccsId[] = #arg;
+#  else
+#   define SCCSID(arg)	static char SccsId[] = "arg";
+#  endif
+# else
+#  define SCCSID(arg)
+# endif
 
 # ifdef LOG
 # include <syslog.h>
